c216c4bbc8487acf6d00d8bbfe0bc2bcfbb9560e
anneraev <47757104+anneraev@users.noreply.github.com>
2019-03-27 15:20:48 -0500

    reupload practice

---
+++ src/scripts/main.js
@@ -0,0 +1,11 @@
+//pet representing object.
+//species property.
+//nicknames array
+//age integer
+const petObject = {
+    name: "Cupcake",
+    species: "?",
+    nicknames: ["ohmygodwhatisthat!?", "getitawayfromme", "imcallinganimalcontrol", "monster", "slimyhellspawn"],
+    age: 1
+}
+console.log(petObject);
